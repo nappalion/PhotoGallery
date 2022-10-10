48fb516325532e79ee212d41bebcf99048d511e3
nipplelion <piolotorrico@yahoo.com>
2022-10-10 01:41:25 -0700

    Get data from Flickr

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.photogallery.api
+
+import com.bignerdranch.android.photogallery.BuildConfig
+import com.bignerdranch.android.photogallery.FlickrResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+            "&api_key=${BuildConfig.FLICKR_API_KEY}" +
+            "&format=json" +
+            "&nojsoncallback=1" +
+            "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
